edb285ae95e3d7a41f1b2abf1e5e657e0b22c47a
Oleksii Lytovchenko <oleksii.lytovchenko@gmail.com>
2020-10-26 01:28:13 +0200

    Replaced control bit function with just an enum

--- ssd1306.h
+++ ssd1306.h
@@ -8,8 +8,6 @@
 
 #define SSD1306_COM_PINS_ALTERNATIVE_CONFIG 0x1
 #define SSD1306_COM_PINS_ENABLE_LR_REMAP    0x1
-#define SSD1306_CTRL_BYTE_CO                0x80
-#define SSD1306_CTRL_BYTE_DATA              0x40
 
 typedef enum SSD1306Cmd {
     // Fundamental commands
@@ -55,9 +53,12 @@ typedef enum SSD1306AddrMode {
     SSD1306_ADDR_MODE_PAGE                  = 0x02,
 } SSD1306AddrMode_t;
 
+typedef enum SSD1306CtrlByte {
+    SSD1306_CTRL_COMMAND                    = 0x00,
+    SSD1306_CTRL_DATA                       = 0x40,
+    SSD1306_CTRL_CO_COMMAND                 = 0x80,
+    SSD1306_CTRL_CO_DATA                    = 0xC0 // 0x80 | 0x40
+} SSD1306CtrlByte_t;
 
-static INLINE uint8_t ssd1306_ctrl_byte(const bool has_more_ctrl, const bool is_data) {
-    return (has_more_ctrl ? SSD1306_CTRL_BYTE_CO : 0x0) | (is_data ? SSD1306_CTRL_BYTE_DATA : 0x0);
-}
 
 #endif //THERMOMETER_SSD1306_H
