87976760f618f7884fa32a92ba272d72e157221c
Oleksii Lytovchenko <oleksii.lytovchenko@gmail.com>
2020-12-21 00:29:36 +0200

    Enabled data initialization in the startup file (removed comments)

--- CMSIS/Device/ST/STM32F1xx/Source/gcc/startup_stm32f103xb.s
+++ CMSIS/Device/ST/STM32F1xx/Source/gcc/startup_stm32f103xb.s
@@ -62,7 +62,7 @@ defined in linker script */
 Reset_Handler:
 
 /* Copy the data segment initializers from flash to SRAM */
-/*  movs r1, #0
+movs r1, #0
   b LoopCopyDataInit
 
 CopyDataInit:
@@ -78,16 +78,16 @@ LoopCopyDataInit:
   cmp r2, r3
   bcc CopyDataInit
   ldr r2, =_sbss
-  b LoopFillZerobss */
+  b LoopFillZerobss
 /* Zero fill the bss segment. */
-/* FillZerobss:
+FillZerobss:
   movs r3, #0
   str r3, [r2], #4
 
 LoopFillZerobss:
   ldr r3, = _ebss
   cmp r2, r3
-  bcc FillZerobss */
+  bcc FillZerobss
 
 /* Call the clock system intitialization function.*/
     bl  SystemInit
