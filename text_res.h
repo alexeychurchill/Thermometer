24af1194cf15a5193a1ee18d8f5df9dd7554d918
Oleksii Lytovchenko <oleksii.lytovchenko@gmail.com>
2021-02-13 02:56:53 +0200

    Add macro for definition of the text resources

--- text_res.h
+++ text_res.h
@@ -4,5 +4,7 @@
 #include <stdint.h>
 
 const uint8_t RES_STR_SCR_TEMP_TITLE[] = "Температура";
+#define RES_TEXT(name, value) static const uint8_t RES_STR_##name[] = value;
+
 
 #endif //THERMOMETER_TEXT_RES_H
