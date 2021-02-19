949d59c6031040cceabda25d9fc8f2646c69edb4
Oleksii Lytovchenko <oleksii.lytovchenko@gmail.com>
2021-02-20 01:46:58 +0200

    Add "Under Construction" screen

--- text_res.h
+++ text_res.h
@@ -7,4 +7,7 @@ const uint8_t RES_STR_SCR_TEMP_TITLE[] = "Температура";
 #define RES_TEXT(name, value) static const uint8_t RES_STR_##name[] = value;
 
 
+RES_TEXT(SCR_CONSTRUCTION_MSG_LINE_0, "У процесі")
+RES_TEXT(SCR_CONSTRUCTION_MSG_LINE_1, "створення")
+
 #endif //THERMOMETER_TEXT_RES_H
