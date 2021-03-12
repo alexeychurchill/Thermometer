6f835a3fbd03e380b580a93e138f03c816119fb0
Oleksii Lytovchenko <oleksii.lytovchenko@gmail.com>
2021-03-13 01:55:36 +0200

    Fix bug with wrong font in the screen title draw function

--- ui/scr_common.c
+++ ui/scr_common.c
@@ -1,16 +1,18 @@
 #include "scr_common.h"
 #include "../ssd1306.h"
 #include "../display.h"
+#include "bebas_16.h"
 
 void scr_common_draw_title(const UiDisplay_t *display, const uint8_t *title_text) {
     uint8_t title_line_y = SCR_COMMON_TITLE_DELIMITER_Y;
     for (uint8_t x = 0; x < DISPLAY_WIDTH; x++) {
-        display -> put_pixel(x, title_line_y);
+        display->put_pixel(x, title_line_y);
     }
 
-    display -> text_set_offset_x(0);
-    display -> text_set_page(0);
-    display -> text_set_align(TEXT_ALIGN_CENTER);
+    display->text_set_font(ssd1306_bebas_16_get_glyph);
+    display->text_set_offset_x(0);
+    display->text_set_page(0);
+    display->text_set_align(TEXT_ALIGN_CENTER);
 
-    display -> put_text(title_text);
+    display->put_text(title_text);
 }
